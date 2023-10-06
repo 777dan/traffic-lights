225daa3850d4825a49010245f683c5f12ee9a53e
777dan <danya.nikitin7@gmail.com>
2023-10-06 16:43:50 +0300

    mixed up the traffic lights

--- script.js
+++ script.js
@@ -10,13 +10,16 @@ function trafficLightsFunc() {
 }
 
 let trafficLight1 = trafficLightsFunc();
-for (let i = 0; i < 5; i++) {
-    let color = trafficLight1();
-    console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
-}
-console.log("-----");
 let trafficLight2 = trafficLightsFunc();
-for (let i = 0; i < 5; i++) {
-    let color = trafficLight2();
-    console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
-}
+let color = trafficLight1();
+console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
+color = trafficLight1();
+console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
+color = trafficLight2();
+console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
+color = trafficLight1();
+console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
+color = trafficLight2();
+console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
+color = trafficLight2();
+console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
