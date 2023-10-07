277a5c280676760ecb77fdbbfe790bbfa45f1945
777dan <danya.nikitin7@gmail.com>
2023-10-07 12:13:33 +0300

    optimized

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+"use strict"
 const colorsArr = ["red", "green", "yellow"];
-function trafficLightsFunc() {
+function trafficLightsFunc(num) {
     let count = 0;
     let color = colorsArr[count];
     let changeColor = function () {
         if (count === 2) count = -1;
-        return color = colorsArr[++count];
+        return [color = colorsArr[++count], num];
     }
     return changeColor;
 }
 
-let trafficLight1 = trafficLightsFunc();
-let trafficLight2 = trafficLightsFunc();
-let color = trafficLight1();
-console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
-color = trafficLight1();
-console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
-color = trafficLight2();
-console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
-color = trafficLight1();
-console.log('%c 1 traffic light ', 'background: ' + color + '; color: white;');
-color = trafficLight2();
-console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
-color = trafficLight2();
-console.log('%c 2 traffic light ', 'background: ' + color + '; color: white;');
+let trafficLight1 = trafficLightsFunc(1);
+let trafficLight2 = trafficLightsFunc(2);
+let trafficLightData = trafficLight1();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
+trafficLightData = trafficLight1();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
+trafficLightData = trafficLight2();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
+trafficLightData = trafficLight1();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
+trafficLightData = trafficLight2();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
+trafficLightData = trafficLight2();
+console.log('%c ' + trafficLightData[1] + ' traffic light ', 'background: ' + trafficLightData[0] + '; color: white;');
